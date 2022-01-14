59a70c8ba6ac3b0963ce992f88a17e29670ce506
Lucien George <lucien.george@shopify.com>
2022-01-14 17:05:39 +0000

    install syntax highlighting

--- install.sh
+++ install.sh
@@ -31,18 +31,6 @@
 #   fi
 # done
 
-# # Install zsh-syntax-highlighting plugin
-# CURRENT_DIR=`pwd`
-# ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-# mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-# if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-#   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-#   git clone https://github.com/zsh-users/zsh-autosuggestions
-#   git clone https://github.com/zsh-users/zsh-syntax-highlighting
-# fi
-
-# cd "$CURRENT_DIR"
-
 # ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
 # mkdir -p "$ZSH_THEMES_DIR" && cd "$ZSH_THEMES_DIR"
 # if [ ! -d "$ZSH_THEMES_DIR/powerlevel10k" ]; then
@@ -86,8 +74,17 @@
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Install zsh-syntax-highlighting plugin
+CURRENT_DIR=`pwd`
+ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting
+fi
 
-# cp ~/dotfiles/.dircolors ~/.dircolors
+cd "$CURRENT_DIR"
+
 cp ~/dotfiles/zshrc ~/.zshrc
-# cp ~/dotfiles/zsh_themes/robbyrussell_with_host.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell_with_host.zsh-theme
 cp ~/dotfiles/p10k.zsh ~/.p10k.zsh
