1ab845f1940c48834e64b5d47ecfc3299479865c
Lucien George <lucien.george@shopify.com>
2022-01-14 16:37:40 +0000

    testing spin setup

--- install.sh
+++ install.sh
@@ -1,86 +1,93 @@
-#!/bin/zsh
+# #!/bin/zsh
+
+# # Define a function which rename a `target` file to `target.backup` if the file
+# # exists and if it's a 'real' file, ie not a symlink
+# backup() {
+#   target=$1
+#   if [ -e "$target" ]; then
+#     if [ ! -L "$target" ]; then
+#       mv "$target" "$target.backup"
+#       echo "-----> Moved your old $target config file to $target.backup"
+#     fi
+#   fi
+# }
+
+# symlink() {
+#   file=$1
+#   link=$2
+#   if [ ! -e "$link" ]; then
+#     echo "-----> Symlinking your new $link"
+#     ln -s $file $link
+#   fi
+# }
+
+# # For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
+# # and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+# for name in aliases gitconfig irbrc rspec zprofile zshrc; do
+#   if [ ! -d "$name" ]; then
+#     target="$HOME/.$name"
+#     backup $target
+#     symlink $PWD/$name $target
+#   fi
+# done
+
+# # Install zsh-syntax-highlighting plugin
+# CURRENT_DIR=`pwd`
+# ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+# mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+# if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+#   echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+#   git clone https://github.com/zsh-users/zsh-autosuggestions
+#   git clone https://github.com/zsh-users/zsh-syntax-highlighting
+# fi
+
+# cd "$CURRENT_DIR"
+
+# ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
+# mkdir -p "$ZSH_THEMES_DIR" && cd "$ZSH_THEMES_DIR"
+# if [ ! -d "$ZSH_THEMES_DIR/powerlevel10k" ]; then
+#   echo "-----> Installing powerlevel10k..."
+#   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+# fi
+
+# cd "$CURRENT_DIR"
+
+# # Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
+# # If it's a macOS
+# if [[ `uname` =~ "Darwin" ]]; then
+#   CODE_PATH=~/Library/Application\ Support/Code/User
+# # Else, it's a Linux
+# else
+#   CODE_PATH=~/.config/Code/User
+#   # If this folder doesn't exist, it's a WSL
+#   if [ ! -e $CODE_PATH ]; then
+#     CODE_PATH=~/.vscode-server/data/Machine
+#   fi
+# fi
+
+# for name in settings.json keybindings.json; do
+#   target="$CODE_PATH/$name"
+#   backup $target
+#   symlink $PWD/$name $target
+# done
+
+# # Symlink SSH config file to the present `config` file for macOS and add SSH passphrase to the keychain
+# if [[ `uname` =~ "Darwin" ]]; then
+#   target=~/.ssh/config
+#   backup $target
+#   symlink $PWD/config $target
+#   ssh-add -K ~/.ssh/id_ed25519
+# fi
+
+# # Refresh the current terminal with the newly installed configuration
+# exec zsh
+
+# echo "👌 Carry on with git setup!"
+
+#!/bin/sh
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Define a function which rename a `target` file to `target.backup` if the file
-# exists and if it's a 'real' file, ie not a symlink
-backup() {
-  target=$1
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      mv "$target" "$target.backup"
-      echo "-----> Moved your old $target config file to $target.backup"
-    fi
-  fi
-}
-
-symlink() {
-  file=$1
-  link=$2
-  if [ ! -e "$link" ]; then
-    echo "-----> Symlinking your new $link"
-    ln -s $file $link
-  fi
-}
-
-# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
-# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
-for name in aliases gitconfig irbrc rspec zprofile zshrc; do
-  if [ ! -d "$name" ]; then
-    target="$HOME/.$name"
-    backup $target
-    symlink $PWD/$name $target
-  fi
-done
-
-# Install zsh-syntax-highlighting plugin
-CURRENT_DIR=`pwd`
-ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting
-fi
-
-cd "$CURRENT_DIR"
-
-ZSH_THEMES_DIR="$HOME/.oh-my-zsh/custom/themes"
-mkdir -p "$ZSH_THEMES_DIR" && cd "$ZSH_THEMES_DIR"
-if [ ! -d "$ZSH_THEMES_DIR/powerlevel10k" ]; then
-  echo "-----> Installing powerlevel10k..."
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
-fi
-
-cd "$CURRENT_DIR"
-
-# Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
-# If it's a macOS
-if [[ `uname` =~ "Darwin" ]]; then
-  CODE_PATH=~/Library/Application\ Support/Code/User
-# Else, it's a Linux
-else
-  CODE_PATH=~/.config/Code/User
-  # If this folder doesn't exist, it's a WSL
-  if [ ! -e $CODE_PATH ]; then
-    CODE_PATH=~/.vscode-server/data/Machine
-  fi
-fi
-
-for name in settings.json keybindings.json; do
-  target="$CODE_PATH/$name"
-  backup $target
-  symlink $PWD/$name $target
-done
-
-# Symlink SSH config file to the present `config` file for macOS and add SSH passphrase to the keychain
-if [[ `uname` =~ "Darwin" ]]; then
-  target=~/.ssh/config
-  backup $target
-  symlink $PWD/config $target
-  ssh-add -K ~/.ssh/id_ed25519
-fi
-
-# Refresh the current terminal with the newly installed configuration
-exec zsh
-
-echo "👌 Carry on with git setup!"
+cp ~/dotfiles/.dircolors ~/.dircolors
+cp ~/dotfiles/zshrc ~/.zshrc
+cp ~/dotfiles/zsh_themes/robbyrussell_with_host.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell_with_host.zsh-theme
